b37859871100984187d2aaa3462db0cd1492ef71
Bill Clinton <BillClinton@users.noreply.github.com>
2015-06-02 16:54:18 -0400

    added two overrides from ext-5.0.1.1255 branch.

---
+++ overrides/dom/Element/ValidId.js
@@ -0,0 +1,14 @@
+/**
+ * Fixes regular expresion used to validate DOM ids to match HTML spec
+ * 
+ * Discussion: http://www.sencha.com/forum/showthread.php?296173-validIdRe-throwing-Invalid-Element-quot-id-quot-for-valid-ids-containing-colons
+ */
+Ext.define('Jarvus.hotfixes.ext.dom.Element.ValidID', {
+    override: 'Ext.dom.Element',
+
+    validIdRe: /^[a-z][a-z0-9\-_:.]*$/i,
+    
+    getObservableId: function() {
+        return (this.observableId = this.callParent().replace(/([.:])/g, "\\$1"));
+    }
+});
