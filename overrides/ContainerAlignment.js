97c439a41d9423d7ff49b8c422264ca2a2601441
Chris Alfano <chris@jarv.us>
2015-09-23 16:41:11 -0400

    Generalize alignment override in hotfix and remove from subpanel class

---
+++ overrides/ContainerAlignment.js
@@ -0,0 +1,26 @@
+/*jslint browser: true, undef: true, laxcomma:true *//*global Ext*/
+/**
+ * Fixes issue where alignTo doesn't take into account scrollable parents or viewports with padding
+ */
+Ext.define('Jarvus.hotfixes.ContainerAlignment', {
+    override: 'Ext.Container',
+
+    getAlignmentInfo: function(component, alignment) {
+        var me = this,
+            alignmentInfo = me.callParent(arguments),
+            scrollableParent = me.up('{getScrollable()}'),
+            scrollable = scrollableParent && scrollableParent.getScrollable(),
+            topOffset = 0;
+
+        if (scrollable) {
+            // shift alignToBox.top by containter padding and scroll position
+            topOffset -= scrollable.getElement().getY() - scrollable.getPosition().y;
+        } else {
+            topOffset -= me.getParent().innerElement.getY();
+        }
+
+        alignmentInfo.stats.alignToBox.top += topOffset;
+
+        return alignmentInfo;
+    }
+});
