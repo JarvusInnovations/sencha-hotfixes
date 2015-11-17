4edc9166ba5a32b287615b8b089433cfd0735139
Chris Alfano <chris@jarv.us>
2015-11-17 12:59:45 -0500

    Add hotfix for issue removing multiple records from store

---
+++ overrides/data/StoreRemovedRecordsRef.js
@@ -0,0 +1,18 @@
+/**
+ * The public method getRemovedRecords returns a reference to the
+ * private array removed, enabling a method that manipulates the
+ * returned array to accidentally modify the private internal
+ * array's state.
+ *
+ * Return a cloned array instead
+ *
+ * Discussion: https://www.sencha.com/forum/showthread.php?301217
+ */
+Ext.define('Jarvus.hotfixes.ext.data.StoreRemovedRecordsRef', {
+    override: 'Ext.data.ProxyStore',
+
+    getRemovedRecords: function() {
+        var removed = this.removed;
+        return removed ? Ext.Array.clone(removed) : removed;
+    },
+});
